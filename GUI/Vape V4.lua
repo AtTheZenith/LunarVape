5de7c0f8c3308196102ae2b47b47e77c943102d8
zenith <157610281+AtTheZenith@users.noreply.github.com>
2026-01-09 06:17:39 +0300

    add volt support

--- GUI/Vape V4.lua
+++ GUI/Vape V4.lua
@@ -24,7 +24,7 @@ local mainapi = {
 }
 
 if identifyexecutor then
-  if table.find({ 'Velocity', 'Zenith', 'Volcano', 'Delta', 'Hydrogen' }, (identifyexecutor())) then
+  if table.find({ 'Velocity', 'Zenith', 'Volcano', 'Delta', 'Hydrogen', 'Volt' }, (identifyexecutor())) then
     mainapi.ThreadFix = false
   end
 end
