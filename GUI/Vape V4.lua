e8cc267e60a097943dde12a01daafd27abb5d059
zenith <157610281+AtTheZenith@users.noreply.github.com>
2026-01-11 05:50:58 +0300

    add potassium support
    i think

--- GUI/Vape V4.lua
+++ GUI/Vape V4.lua
@@ -24,7 +24,7 @@ local mainapi = {
 }
 
 if identifyexecutor then
-  if table.find({ 'Velocity', 'Zenith', 'Volcano', 'Delta', 'Hydrogen', 'Volt', 'Cryptic-Windows' }, (identifyexecutor())) then
+  if table.find({ 'Velocity', 'Zenith', 'Volcano', 'Delta', 'Hydrogen', 'Volt', 'Cryptic-Windows', 'Potassium' }, (identifyexecutor())) then
     mainapi.ThreadFix = false
   end
 end
